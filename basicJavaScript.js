530d8a783ce2ab6c3afc996668fa3c6319b97dd7
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-31 23:56:40 +0200

    solved a question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -284,5 +284,19 @@ function nextInLine(arr, item) {
   console.log("After: " + JSON.stringify(testArr));
   
 //-----------------------
+// Understanding Boolean Values
 
+/* Question
+Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+*/
 
+function welcomeToBooleans() {
+
+    // Only change code below this line.
+  
+    return true; // Change this line
+  
+    // Only change code above this line.
+}
+
+console.log(welcomeToBooleans())
