07a9257e293a77ae1451c7536be9332413925bd1
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-01 00:01:47 +0200

    completed a question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -300,3 +300,32 @@ function welcomeToBooleans() {
 }
 
 console.log(welcomeToBooleans())
+
+/* Question
+Create an if statement inside the function to return "Yes, that was true" if the parameter wasThatTrue is true and return "No, that was false" otherwise.
+*/
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+      return "Yes, it's true";
+    }
+    return "No, it's false";
+}
+  
+// Setup
+function trueOrFalse(wasThatTrue) {
+  
+    // Only change code below this line.
+    if(wasThatTrue) {
+        return "Yes that was true";
+    }
+    return "No, that was false";
+    // Only change code above this line.
+}
+  
+// Change this value to test
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
+
+//---------------
