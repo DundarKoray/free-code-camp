0c316ebd21b10fbe2fe8b0a6da5ecc3aa629ff25
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-31 18:44:55 +0200

    completed a question

--- index.js
+++ index.js
@@ -81,4 +81,22 @@ function reusableFunction() {
 reusableFunction()
 
 //-----------------
+// Passing Values to Functions with Arguments
+
+/* Question 
+1. Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+2.Call the function with two numbers as arguments.
+*/
+
+// Example
+function ourFunctionWithArgs(a, b) {
+    console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
   
+// Only change code below this line.
+function functionWithArgs(num1, num2) {
+    console.log(num1 + num2)
+}
+functionWithArgs(25, 10) // Outputs 35
+
