f2d79038e3a2c1ee9de643b5db7523837880a761
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-31 23:25:37 +0200

    solved a question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -259,7 +259,30 @@ processed = processArg(7)
 console.log(processed) // 2
 
 //--------------
+// Stand in Line
 
+/* Question
+Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+Add the number to the end of the array, then remove the first element of the array.
+The nextInLine function should then return the element that was removed.
+*/
+
+function nextInLine(arr, item) {
+    // Your code here
+    arr.push(item);
+    var elementThatIsRemoved =arr.shift()
+
+    return elementThatIsRemoved;  // Change this line
+  }
   
+  // Test Setup
+  var testArr = [1,2,3,4,5];
+  
+  // Display Code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6)); // Modify this line to test
+  console.log("After: " + JSON.stringify(testArr));
+  
+//-----------------------
 
 
