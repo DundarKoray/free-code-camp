715f1faf96eec732a631ad3767862e45f2e4776f
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-31 18:35:15 +0200

    completed a question

--- index.js
+++ index.js
@@ -39,3 +39,21 @@ myArray2.shift();
 // Only change code below this line.
 myArray2.unshift(["Paul", 35])
 console.log(myArray2)
+
+//---------------------
+// Shopping List
+
+/* Question
+Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+
+There should be at least 5 sub-arrays in the list.
+*/
+
+var myList = [];
+myList = [["Garlic Bread", 1], ["Tomato", 5], ["Egg", 12], ["Sliced Salmon", 6], ["Cream Cheese", 1]]
+
+//---------------
