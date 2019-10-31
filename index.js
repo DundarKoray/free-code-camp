01be9345a34ba8744f42d8f0d1aa28247794ec18
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-31 18:00:21 +0200

    completed a question

--- index.js
+++ index.js
@@ -17,5 +17,25 @@ var myArray = [["John", 23], ["dog", 3]];
 // Only change code below this line.
 var removedFromMyArray;
 removedFromMyArray = myArray.shift()
+console.log(removedFromMyArray)
 
 //--------------------
+// Manipulate Arrays With unshift()
+
+/* Question
+Add ["Paul",35] to the beginning of the myArray variable using unshift().
+*/
+
+// Example
+var ourArray2 = ["Stimpson", "J", "cat"];
+ourArray2.shift(); // ourArray now equals ["J", "cat"]
+ourArray2.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray2 = [["John", 23], ["dog", 3]];
+myArray2.shift();
+
+// Only change code below this line.
+myArray2.unshift(["Paul", 35])
+console.log(myArray2)
