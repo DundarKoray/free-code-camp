454885c28f0d3c8462dc4e2e483e9c05b048c09d
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-02 20:05:34 +0200

    solved a qustion

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -612,3 +612,34 @@ function orderMyLogic(val) {
 
 // Change this value to test
 console.log(orderMyLogic(7)); // Less than 10
+
+/*Question
+Write chained if/else if statements to fulfill the following conditions:
+
+num < 5 - return "Tiny"
+num < 10 - return "Small"
+num < 15 - return "Medium"
+num < 20 - return "Large"
+num >= 20 - return "Huge"
+*/
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny"
+  } else if (num < 10) {
+    return "Small"
+  } else if (num < 15) {
+    return "Medium"
+  } else if (num < 20) {
+    return "Large"
+  } else if (num >= 20) {
+    return "Huge"
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(7);
