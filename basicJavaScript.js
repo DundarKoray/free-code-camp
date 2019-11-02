e89ab5f5f10e238ac5bb257f3300410c330a4817
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-02 17:57:55 +0200

    solved a question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -464,3 +464,24 @@ function testGreaterOrEqual(val) {
 // Change this value to test
 console.log(testGreaterOrEqual(10)); // 10 or Over
 
+//--------------------
+// Comparison with the Less Than Operator
+
+/* Question
+Add the less than operator to the indicated lines so that the return statements make sense.
+*/
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+console.log(testLessThan(10)); // Under 25
