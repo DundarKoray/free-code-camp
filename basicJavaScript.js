e7d77bdb569e35d6526c480d9ebd5c1025e91ac0
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-02 19:49:23 +0200

    solved a question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -549,3 +549,29 @@ function testLogicalOr(val) {
 
 // Change this value to test
 console.log(testLogicalOr(15)); // Inside
+
+//--------------------
+// Introducing Else Statements
+
+/* Question
+Combine the if statements into a single if/else statement.
+*/
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+console.log(testElse(4)); // 5 or Smaller
