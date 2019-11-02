abeaf8c1d55b4e734aa00118e4e94d83c3d51001
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-02 21:01:48 +0200

    completed a question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -716,3 +716,41 @@ function caseInSwitch(val) {
 
 // Change this value to test
 console.log(caseInSwitch(1)); // alpha
+
+//--------------------
+// Adding a Default Option in Switch Statements
+
+/* Question
+Write a switch statement to set answer for the following conditions:
+"a" - "apple"
+"b" - "bird"
+"c" - "cat"
+default - "stuff"
+*/
+
+function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case "a":
+      answer = "apple"
+      break;
+    case "b":
+      answer = "bird"
+      break;
+    case "c":
+      answer = "cat"
+      break;
+    default:
+      answer = "stuff"
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+console.log(switchOfStuff(1)); // stuff
+console.log(switchOfStuff("c")); // cat
