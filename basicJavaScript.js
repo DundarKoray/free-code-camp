97106f2ae7e2742b2bb1719826ac274138c883e8
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-05 10:46:14 +0200

    solved a question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -756,7 +756,7 @@ console.log(switchOfStuff(1)); // stuff
 console.log(switchOfStuff("c")); // cat
 
 //--------------------
-// Adding a Default Option in Switch Statements
+// Multiple Identical Options in Switch StatementsPassed
 
 /* Question
 Write a switch statement to set answer for the following ranges:
@@ -796,3 +796,40 @@ function sequentialSizes(val) {
 // Change this value to test
 console.log(sequentialSizes(1)); // Low
 console.log(sequentialSizes(8)); // High
+
+//--------------------
+// Replacing If Else Chains with Switch
+
+/* Question
+Change the chained if/else if statements into a switch statement. "High"
+*/
+
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+console.log(chainToSwitch(7)); // Ate Nine
+console.log(chainToSwitch("bob")); // Marley
