b03fad899304973279dc431985e309113fe9aa63
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-07 11:46:54 +0200

    solved a question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -1213,3 +1213,26 @@ var myMusic = [
     ]
   }
 ];
+
+//--------------------
+// Accessing Nested Objects
+
+/* Question
+Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use dot notation for all properties where possible, otherwise use bracket notation.
+*/
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+console.log(gloveBoxContents) // maps
