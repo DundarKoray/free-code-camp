9eb4cc6c8cd17dba64df55e9209731b6f4aa6fbb
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-07 21:44:19 +0200

    completed question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -1464,3 +1464,25 @@ for(var i = 0; i < myArr7.length; i++) {
 }
 
 console.log(total) // 20
+
+//--------------------
+// Nesting For Loops
+
+/* Question
+Modify function multiplyAll so that it multiplies the product variable by each number in the sub-arrays of arr
+*/
+
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i = 0; i < arr.length; i++){
+    for(var j = 0; j < arr[i].length; j++){
+      product *= arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
+}
+
+         
+console.log(multiplyAll([[1,2],[3,4],[5,6,7]])) //5040
