8d2f6b1ad91f56b179796b4391173a056a585d60
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-08 12:09:39 +0200

    started the new section

---
+++ es6.js
@@ -0,0 +1,8 @@
+console.log("--------HI FROM ES6----------")
+//--------------------
+// Explore Differences Between the var and let Keywords
+
+/* Question
+Update the code so it only uses the let keyword.
+*/
+
