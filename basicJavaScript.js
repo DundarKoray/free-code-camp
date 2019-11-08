655f56aa1331c38d218aabd9d443a20f2de72871
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-08 12:03:19 +0200

    completed Basic JavaScript

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -1681,3 +1681,39 @@ function checkSign(num) {
 console.log(checkSign(10)); // positive
 console.log(checkSign(-10)); // negative
 console.log(checkSign(0)); // zero
+
+//--------------------
+// Use Recursion to Create a Range of Numbers
+
+/* Question
+We have defined a function named rangeOfNumbers2 with two parameters. The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter. The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
+*/
+
+function rangeOfNumbers2(startNum, endNum) {
+  if (endNum - startNum === 0) {
+    return [startNum];
+  } else {
+    var numbers = rangeOfNumbers2(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
+
+//--------------------
+// Use Recursion to Create a Countdown
+
+/* Question
+We have defined a function called countdown with two parameters. The function should take an array in the myArray parameter and append the numbers n through 1 based on the n parameter.
+For example, calling this function with n = 5 will pad the array with the numbers [5, 4, 3, 2, 1] inside of it. Your function must use recursion by calling itself and must not use loops of any kind.
+*/
+
+//Only change code below this line
+function countdown(myArray, n) {
+  if (n <= 0) {
+    return;
+  }
+  else {
+    myArray.push(n);
+    countdown(myArray, n - 1);
+  }
+}
