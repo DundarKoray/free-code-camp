8a6cd8d7b2b737f5633cace6a681e2cda3a7bc09
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-08 11:35:31 +0200

    completed another question

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -1652,3 +1652,17 @@ function convertToInteger2(str) {
 }
 
 console.log(convertToInteger2("10011"));
+
+//--------------------
+// Use the Conditional (Ternary) Operator
+
+/* Question
+Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either "Equal" or "Not Equal".
+*/
+
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
+}
+
+console.log(checkEqual(1, 2)); // Not Equal
+console.log(checkEqual(2, 2)); // Equal
