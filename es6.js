2ff4e761725e385afdfa13efdc9024d69fd88339
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-08 13:03:05 +0200

    solved a problem

--- es6.js
+++ es6.js
@@ -104,3 +104,16 @@ function freezeObj() {
     return MATH_CONSTANTS.PI;
 }
 const PI = freezeObj();
+
+//--------------------
+// Use Arrow Functions to Write Concise Anonymous Functions
+
+/* Question
+Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. Also make sure nothing is defined using the keyword var.
+*/
+
+const magic = () => {
+    "use strict";
+    return new Date();
+};
+
