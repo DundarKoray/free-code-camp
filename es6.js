16ba839aae96fcd44414a1e0fb12ad207794c616
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-12 00:00:26 +0200

    solved a question

--- es6.js
+++ es6.js
@@ -176,3 +176,28 @@ let arr2;
 arr2 = [...arr1];  // change this line
 
 console.log(arr2); // (5) ["JAN", "FEB", "MAR", "APR", "MAY"]
+
+//--------------------
+// Use Destructuring Assignment to Extract Values from Objects
+
+/* Question
+Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables today and tomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+*/
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // change code below this line
+  
+//   const today = HIGH_TEMPERATURES.today;
+const {today, tomorrow} = HIGH_TEMPERATURES
+//   const tomorrow = HIGH_TEMPERATURES.tomorrow;
+  
+  // change code above this line
+  
+  //console.log(yesterday) // should be not defined
+  console.log(today); // should be 77
+  console.log(tomorrow); // should be 80
