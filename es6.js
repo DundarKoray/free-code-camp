ce8494443e6f95a74c70eb3ec21b3731c1bd2c25
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-12 14:59:45 +0200

    solved a question

--- es6.js
+++ es6.js
@@ -252,5 +252,19 @@ const {today:{ low: lowToday2, high: highToday2 }} = LOCAL_FORECAST
   
 // change code above this line
   
-  console.log(lowToday2); // should be 64
-  console.log(highToday2); // should be 77
+console.log(lowToday2); // should be 64
+console.log(highToday2); // should be 77
+
+//--------------------
+// Use Destructuring Assignment to Assign Variables from Arrays
+
+/* Question
+Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+*/
+
+let a = 8, b = 6;
+// change code below this line
+[b, a] = [8, 6]
+// change code above this line
+console.log(a); // should be 6
+console.log(b); // should be 8
