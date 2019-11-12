8c6d4ccca993cfb9dbe566d06bed9e060ccd0e89
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-12 21:52:30 +0200

    solved a question

--- regularExpressions.js
+++ regularExpressions.js
@@ -11,4 +11,17 @@ let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result2 = myRegex.test(myString); // Change this line
 
-console.log(result2) // true
+console.log("Does Hello exists in myString?: ", result2) // true
+
+//--------------------
+// Match Literal Strings
+
+/* Question
+Complete the regex waldoRegex to find "Waldo" in the string waldoIsHiding with a literal match.
+*/
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result3 = waldoRegex.test(waldoIsHiding); // true
+
+console.log(result3)
