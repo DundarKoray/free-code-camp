8d45653e2a0017076e9acc0f2c093e9bff8b5c49
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-12 21:47:58 +0200

    started RegularExpressions and solved the first q

---
+++ regularExpressions.js
@@ -0,0 +1,14 @@
+console.log("--------HI FROM REGULAR EXPRESSIONS----------")
+
+//--------------------
+// Using the Test Method
+
+/* Question
+Apply the regex myRegex on the string myString using the .test() method.
+*/
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result2 = myRegex.test(myString); // Change this line
+
+console.log(result2) // true
