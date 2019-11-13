7be4fc691ec283cfbf1af47072d354caaa3d4112
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-13 18:27:38 +0200

    solved a question

--- regularExpressions.js
+++ regularExpressions.js
@@ -285,3 +285,23 @@ let noNumRegex = /\D/g; // Change this line
 let result22 = movieName1.match(noNumRegex).length;
 
 console.log(result22) // 17
+
+//--------------------
+// Restrict Possible Usernames
+
+/* Question
+Change the regex userCheck to fit the constraints listed above.
+
+Notes
+1) Usernames can only use alpha-numeric characters.
+
+2) The only numbers in the username have to be at the end. There can be zero or more of them at the end.
+
+3) Username letters can be lowercase and uppercase.
+
+4) Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+*/
+
+let username = "JackOfAllTrades";
+const userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
+let result = userCheck.test(username);
