6fcfd0c44bd4f23c9158a45fb640e93e4c5529dd
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-13 13:23:25 +0200

    solved a question

--- regularExpressions.js
+++ regularExpressions.js
@@ -93,3 +93,20 @@ let unRegex = /un./; // Change this line
 let result8 = unRegex.test(exampleStr);
 
 console.log(result8) //true
+
+//--------------------
+// Match Single Character with Multiple Possibilities
+
+/* Question
+Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample.
+
+Note
+Be sure to match both upper- and lowercase vowels.
+*/
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result9 = quoteSample.match(vowelRegex); // Change this line
+
+console.log(result9)
+//(25) ["e", "a", "e", "o", "u", "i", "e", "a", "o", "e", "o", "e", "I", "a", "e", "o", "o", "e", "i", "o", "e", "o", "i", "e", "i"]
