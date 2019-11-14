bc7253009ad93ca5fed39600388dc3ea6e826a5e
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-14 23:55:19 +0200

    solved a question

--- basicDataStructures.js
+++ basicDataStructures.js
@@ -12,3 +12,28 @@ We have defined a variable called yourArray. Complete the statement by assigning
 let yourArray = ["This", "is", "a", true, "story", "based", "in Helsinki", 1993] // change this line
 
 console.log(yourArray.length) //8
+
+//--------------------
+// Access an Array's Contents Using Bracket Notation
+
+//let yourArray; // change this line
+
+/*
+let myArray = ["a", "b", "c", "d"];
+// change code below this line
+
+//change code above this line
+console.log(myArray);
+*/
+
+/* Question
+In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides "b".
+*/
+
+let myArray5 = ["a", "b", "c", "d"];
+// change code below this line
+myArray5[1] = "BRO!"
+//change code above this line
+
+console.log(myArray5); //(4) ["a", "BRO!", "c", "d"]
+
