922164c9d9b3ebfb7879c991a3845d5c9f17c1ed
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-14 13:30:52 +0200

    solved a question

--- debugging.js
+++ debugging.js
@@ -154,3 +154,30 @@ console.log(result34); // 9
 
 // Solution Note: function was called wrong, changed getNine to getNine()
 
+
+//--------------------
+// Catch Arguments Passed in the Wrong Order When Calling a Function
+
+/*
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(exp, base);
+console.log(power);
+*/
+
+/* Question
+The function raiseToPower raises a base to an exponent. Unfortunately, it's not called properly - fix the code so the value of power is the expected 8.
+*/
+
+function raiseToPower(b, e) {
+    return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power); // 8
