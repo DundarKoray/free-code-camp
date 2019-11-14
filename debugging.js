495365596cddaf86d72f2616c69cc634935db5d8
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-14 12:29:07 +0200

    completed the first challange in debugging

--- debugging.js
+++ debugging.js
@@ -1,1 +1,18 @@
 console.log("Hello from Debugging.")
+
+//--------------------
+// Use the JavaScript Console to Check the Value of a Variable
+
+/* Question
+Use the console.log() method to print the value of the variable a where noted in the code.
+*/
+
+let a1 = 5;
+let b1 = 1;
+a1++;
+// Add your code below this line
+
+
+let sumAB = a + b1;
+console.log(sumAB); // 7
+console.log(a1) // 6
