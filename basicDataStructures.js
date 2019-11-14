17d69552b16af96fa48527bf969b3701e5f02fce
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-14 23:41:23 +0200

    solved first question in basicdatastructures sec

--- basicDataStructures.js
+++ basicDataStructures.js
@@ -1,1 +1,14 @@
 console.log("--------HELLO FROM BASIC DATA STRUCTURES----------")
+
+//--------------------
+// Use an Array to Store a Collection of Data
+
+//let yourArray; // change this line
+
+/* Question
+We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+*/
+
+let yourArray = ["This", "is", "a", true, "story", "based", "in Helsinki", 1993] // change this line
+
+console.log(yourArray.length) //8
