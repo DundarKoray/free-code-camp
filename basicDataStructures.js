276deb94728c154e71d8ca8503d9b1a21c73a216
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-15 16:06:29 +0200

    solved a question

--- basicDataStructures.js
+++ basicDataStructures.js
@@ -74,3 +74,23 @@ function popShift(arr) {
 console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
 //(2)["challenge", "complete"]
+
+
+//--------------------
+// Remove Items Using splice()
+
+
+/* Question
+We've defined a function, sumOfTen, which takes an array as an argument and returns the sum of that array's elements. Modify the function, using splice(), so that it returns a value of 10.
+*/
+
+function sumOfTen(arr) {
+    // change code below this line
+    arr.splice(1, 2);
+    // change code above this line
+    return arr.reduce((a, b) => a + b);
+}
+
+// do not change code below this line
+console.log(sumOfTen([2, 5, 1, 5, 2, 1]));
+// 10
