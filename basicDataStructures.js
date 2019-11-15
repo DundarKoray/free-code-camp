e593ba738c96147b2a93c4ed69763fa8b3552ae7
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-15 16:53:54 +0200

    solved a question!

--- basicDataStructures.js
+++ basicDataStructures.js
@@ -214,3 +214,12 @@ function filteredArray(arr, elem) {
 
 // change code here to test different cases:
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9], [20, 4]], 3));
+
+//--------------------
+// Create complex multi-dimensional arrays
+
+
+
+/* Question
+We have defined a variable, myNestedArray, set equal to an array. Modify myNestedArray, using any combination of strings, numbers, and booleans for data elements, so that it has exactly five levels of depth (remember, the outer-most array is level 1). Somewhere on the third level, include the string 'deep', on the fourth level, include the string 'deeper', and on the fifth level, include the string 'deepest'.
+*/
