98632a268ad4ccba0fd76c754bcbeb872345c0ac
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-15 16:17:08 +0200

    solved a question

--- basicDataStructures.js
+++ basicDataStructures.js
@@ -113,4 +113,22 @@ function htmlColorNames(arr) {
 // do not change code below this line
 console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick']));
 
-(5)["DarkSalmon", "BlanchedAlmond", "LavenderBlush", "PaleTurqoise", "FireBrick"]
+//(5)["DarkSalmon", "BlanchedAlmond", "LavenderBlush", "PaleTurqoise", "FireBrick"]
+
+//--------------------
+// Copy Array Items Using slice()
+
+
+/* Question
+We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the elements 'warm' and 'sunny'.
+*/
+
+function forecast(arr) {
+    // change code below this line
+    let newArr = arr.slice(2, 4)
+
+    return newArr;
+}
+
+// do not change code below this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
