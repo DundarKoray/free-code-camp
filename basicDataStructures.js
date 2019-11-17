2796ad1c13cf07e35f1a32549752d16776c96ca1
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-17 16:09:00 +0200

    solved a question and completed entire section

--- basicDataStructures.js
+++ basicDataStructures.js
@@ -460,4 +460,41 @@ let users2 = {
     // change code above this line
   }
   
-  console.log(getArrayOfUsers(users2));
+  console.log(getArrayOfUsers(users2)); // (4) ["Alan", "Jeff", "Sarah", "Ryan"]
+
+//--------------------
+// Modify an Array Stored in an Object
+
+
+/* Question
+Take a look at the object we've provided in the code editor. The user object contains three keys. The data key contains five keys, one of which contains an array of friends. From this, you can see how flexible objects are as data structures. We've started writing a function addFriend. Finish writing it so that it takes a user object and adds the name of the friend argument to the array stored in user.data.friends and returns that array.
+*/
+
+let user3 = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+      username: 'kennethCodesAllDay',
+      joinDate: 'March 26, 2016',
+      organization: 'freeCodeCamp',
+      friends: [
+        'Sam',
+        'Kira',
+        'Tomo'
+      ],
+      location: {
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'USA'
+      }
+    }
+  };
+  
+  function addFriend(userObj, friend) {
+    // change code below this line
+    userObj.data.friends.push(friend);
+    return userObj.data.friends
+    // change code above this line
+  }
+  
+  console.log(addFriend(user3, 'Pete')); // (4) ["Sam", "Kira", "Tomo", "Pete"]
