88098e6dfa7359bf247a97e1c6b3aa716f24b162
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-19 17:51:39 +0200

    solved a question

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -323,7 +323,7 @@ console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
 // [4, 1, 2, 3, 5, 6]
 
 //--------------------
-// Slice and Splice
+// Falsy Bouncer
 
 /* Question
 Remove all falsy values from an array.
@@ -356,3 +356,40 @@ function bouncer2(arr) {
 
 console.log(bouncer2([7, "ate", "", false, 9])); // (3) [7, "ate", 9]
 console.log(bouncer2([undefined, null, 0, "", false, NaN])); // [ ]
+
+
+//--------------------
+// Where do I Belong
+
+/* Question
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+*/
+
+
+function getIndexToIns(arr, num) {
+    // Find my place in this sorted array.
+   
+    arr.sort((a,b) => {
+        return a - b;
+    })
+    
+    for ( let i = 0; i < arr.length; i++){
+        if(arr[i] >= num){
+            return i
+        }
+    }
+    
+    return arr.length
+  }
+  
+  console.log(getIndexToIns([40, 60], 50)); //1
+  console.log(getIndexToIns([40, 60, 70], 65)); //2
+  console.log(getIndexToIns([40, 60, 70], 40)); //2
+  console.log(getIndexToIns([40, 60, 70], 90)); //2
+
+
+ 
