cac7673ce5be547b558acc296ec7973829bb33b3
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-19 16:49:00 +0200

    solved a question

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -322,4 +322,27 @@ function frankenSplice(arr1, arr2, n) {
 console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
 // [4, 1, 2, 3, 5, 6]
 
+//--------------------
+// Slice and Splice
+
+/* Question
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+*/
+
+function bouncer(arr) {
+
+    let newArray = []
+    for ( let i = 0; i < arr.length; i++) {
+         if(arr[i]){
+             newArray.push(arr[i])
+         }
+    }
+
+    return newArray
+}
   
+console.log(bouncer([7, "ate", "", false, 9])); // (3) [7, "ate", 9]
