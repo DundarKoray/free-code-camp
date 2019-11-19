4972264275a6a988fd38c83fef37b2f41e8c7f67
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-19 10:31:39 +0200

    solved a question

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -218,3 +218,23 @@ function truncateString(str, num) {
 }
 
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+
+
+
+// Finders Keepers
+
+/* Question
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+*/
+
+function findElement(arr, func) {
+    let num = 0;
+    for (let i = 0; i < arr.length; i++){
+        num = arr[i];
+        if(func(num)) {
+            return num
+        }
+    }
+  }
+  
+  findElement([1, 2, 3, 4], num => num % 2 === 0);
