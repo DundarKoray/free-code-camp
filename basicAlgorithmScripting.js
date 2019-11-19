76ebfa2fddef81955aa04cc3152d7269a05e4e0e
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-19 11:36:04 +0200

    solved a question

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -299,4 +299,27 @@ console.log(titleCase("I'm a little tea pot"));
 // I'm A Little Tea Pot
 
 
+//--------------------
+// Slice and Splice
 
+/* Question
+You are given two arrays and an index.
+
+Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+*/
+
+
+function frankenSplice(arr1, arr2, n) {
+    let localArr = arr2.slice();
+    localArr.splice(n, 0, ...arr1);
+    return localArr;
+}
+  
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+// [4, 1, 2, 3, 5, 6]
+
+  
