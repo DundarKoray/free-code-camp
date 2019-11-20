25861eae4c381b42695addfc68ac53a74cc53002
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-20 12:44:12 +0200

    solved the first q in object orien. prog. section

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -1,1 +1,18 @@
 console.log("--------HELLO FROM OBJECT ORIENTED PROGRAMMING----------")
+
+
+//--------------------
+// Create a Basic JavaScript Object
+
+
+/* Question
+Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
+*/
+
+let dog = {
+    name: "Tarzan",
+    numLegs: 4
+};
+
+console.log(dog.name) // Tarzan
+console.log(dog.numLegs) // 4
