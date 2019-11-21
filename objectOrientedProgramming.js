a3047adb8300d95ae4be26ae7daffc4f68b286ef
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-21 14:22:07 +0200

    solved a question

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -70,3 +70,20 @@ let myDog1 = new Dog();
 console.log(myDog1); // Dog {name: "Tarzan", color: "brown", numLegs: 4}
 
 console.log(myDog1.name); // Tarzan
+
+//--------------------
+// Extend Constructors to Receive Arguments
+
+
+/* Question
+Create another Dog constructor. This time, set it up to take the parameters name and color, and have the property numLegs fixed at 4. Then create a new Dog saved in a variable terrier. Pass it two strings as arguments for the name and color properties.
+*/
+
+function Dog1(name, color) {
+    this.name = name;
+    this.color = color;
+}
+
+let terrier = new Dog1("cici", "white");
+console.log(terrier.name) // cici
+console.log(terrier.color) // white
