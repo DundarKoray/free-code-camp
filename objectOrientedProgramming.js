37b955141e6a21a79f0a2551538091d5c28b0a6f
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-21 14:24:56 +0200

    solved a question

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -79,11 +79,13 @@ console.log(myDog1.name); // Tarzan
 Create another Dog constructor. This time, set it up to take the parameters name and color, and have the property numLegs fixed at 4. Then create a new Dog saved in a variable terrier. Pass it two strings as arguments for the name and color properties.
 */
 
-function Dog1(name, color) {
+function Dog1(name, color, numLegs) {
     this.name = name;
     this.color = color;
+    this.numLegs = 4;
 }
 
 let terrier = new Dog1("cici", "white");
 console.log(terrier.name) // cici
 console.log(terrier.color) // white
+console.log(terrier.numLegs) // 4
