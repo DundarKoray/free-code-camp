e830f6c30ba253cecdfef1d347e14863f5d40cd7
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-27 13:27:53 +0200

    solved a question

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -794,4 +794,19 @@ const squareList = (arr) => {
 
 // test your code
 const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
-console.log(squaredIntegers);
+console.log(squaredIntegers); // (2) [25, 9]
+
+//--------------------
+// Sort an Array Alphabetically using the sort Method
+
+/* Question
+Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order.
+*/
+
+function alphabeticalOrder(arr) {
+    // Add your code below this line
+    return arr.sort()
+
+    // Add your code above this line
+}
+console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"])); // (6) ["a", "a", "c", "d", "g", "z"]
