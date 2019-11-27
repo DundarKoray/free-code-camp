ae61c8570a8eb9df427520cb2d53dc3804b989c4
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-27 12:06:12 +0200

    solved a question

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -617,3 +617,21 @@ var second = [4, 5];
 console.log(nonMutatingConcat(first, second));
 //(5) [1, 2, 3, 4, 5]
 
+//--------------------
+// Add Elements to the End of an Array Using concat Instead of push
+
+/* Question
+Change the nonMutatingPush function so it uses concat to add newItem to the end of original instead of push. The function should return an array.
+*/
+
+function nonMutatingPush(original, newItem) {
+    // Add your code below this line
+    return original.concat(newItem);
+
+    // Add your code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+
+console.log(nonMutatingPush(first, second));
+//(5) [1, 2, 3, 4, 5]
