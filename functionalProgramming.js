a0b451ed6d9c8c2d0195b2b15f1c83dfcc3b6606
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-27 13:34:02 +0200

    solved a question

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -810,3 +810,40 @@ function alphabeticalOrder(arr) {
     // Add your code above this line
 }
 console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"])); // (6) ["a", "a", "c", "d", "g", "z"]
+
+//--------------------
+// Return a Sorted Array Without Changing the Original Array
+
+/* Question
+Use the sort method in the nonMutatingSort function to sort the elements of an array in ascending order. The function should return a new array, and not mutate the globalArray variable.
+*/
+
+var globalArray = [5, 6, 3, 2, 9];
+function MutatingSort(arr) {
+    // Add your code below this line
+    return arr.sort()
+
+    // Add your code above this line
+}
+console.log(MutatingSort(globalArray));
+//(5)[2, 3, 5, 6, 9]
+
+console.log(globalArray)
+//(5) [2, 3, 5, 6, 9] ORIGINAL ARRAY IS MUTATED!
+
+
+// lETS DO THE SAME AS ABOVE BUT THIS TIME WITHOUT MUTATING THE ORIGINAL ARRAY
+var globalArray1 = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+    // Add your code below this line
+    let newArray = [...arr];
+    return newArray.sort()
+
+    // Add your code above this line
+}
+console.log(nonMutatingSort(globalArray1));
+//(5)[2, 3, 5, 6, 9]
+
+console.log(globalArray1)
+//(5) [5, 6, 3, 2, 9] ORIGINAL ARRAY IS NOT MUTATED
+
