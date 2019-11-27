4344ec0349ef166b1a8862887cd839f5671cd91c
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-27 12:03:15 +0200

    solved a question

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -598,3 +598,22 @@ console.log(MutatingSplice(inputCities1));
 console.log(inputCities1)
 //["Chicago", "Delhi", "Islamabad"] original array mutated!
 
+//--------------------
+// Combine Two Arrays Using the concat Method
+
+/* Question
+Use the concat method in the nonMutatingConcat function to concatenate attach to the end of original. The function should return the concatenated array.
+*/
+
+function nonMutatingConcat(original, attach) {
+    // Add your code below this line
+    return original.concat(attach)
+
+    // Add your code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+
+console.log(nonMutatingConcat(first, second));
+//(5) [1, 2, 3, 4, 5]
+
