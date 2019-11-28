e8a510e7453a04cbf7aaaaf25a487be2aae012b7
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-28 15:28:16 +0200

    solved a question

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -61,7 +61,7 @@ function diffArray(arr1, arr2) {
     
   }
   
-  console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+  console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])); // [4]
 
 
 //--------------------
@@ -89,3 +89,36 @@ function destroyer(arr) {
   }
   
   destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+//--------------------
+// Wherefore art thou
+
+
+/* Question
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+*/
+
+function whatIsInAName(collection, source) {
+    // What's in a name?
+    var srcKeys = Object.keys(source);
+    console.log(srcKeys)
+    // Only change code below this line
+    return collection.filter((obj) => {
+        return srcKeys.map((key)=> {
+            return obj.hasOwnProperty(key) && obj[key] === source[key]
+        }).reduce((a,b)=> {
+            return a && b
+        })
+    })
+
+  
+    // Only change code above this line
+    
+  }
+  
+  console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+
