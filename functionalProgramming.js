0bfc7125d595070100378e5e93f3f59f0cff9905
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-28 12:21:48 +0200

    solved a question

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -904,3 +904,20 @@ function urlSlug(title) {
 // Add your code above this line
 
 console.log(urlSlug(globalTitle)); // Should be "winter-is-coming"
+
+
+//--------------------
+// Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+/* Question
+Use the every method inside the checkPositive function to check if every element in arr is positive. The function should return a Boolean value.
+*/
+
+function checkPositive(arr) {
+    // Add your code below this line
+    return arr.every(item => item > 0)
+  
+    // Add your code above this line
+  }
+  console.log(checkPositive([1, 2, 3, -4, 5])); // false
+  console.log(checkPositive([1, 2, 3, 4, 5])); // true
