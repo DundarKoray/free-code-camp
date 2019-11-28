ab7c2f32189be240856206a20934bed3cbb85ee8
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-28 12:39:41 +0200

    solved a question

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -938,3 +938,22 @@ function checkPositive1(arr) {
     // Add your code above this line
   }
   console.log(checkPositive1([1, 2, 3, -4, 5])); // true
+
+
+//--------------------
+// Introduction to Currying and Partial Application
+
+/* Question
+Fill in the body of the add function so it uses currying to add parameters x, y, and z.
+*/
+
+function add(x) {
+    // Add your code below this line
+    return function(y) {
+      return function(z) {
+        return x + y + z;
+      };
+    };
+    // Add your code above this line
+  }
+  add(10)(20)(30);
