156f03376900d08833b7afec3ddb1abc1dba0e8e
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-28 12:36:48 +0200

    solved a question

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -921,3 +921,20 @@ function checkPositive(arr) {
   }
   console.log(checkPositive([1, 2, 3, -4, 5])); // false
   console.log(checkPositive([1, 2, 3, 4, 5])); // true
+
+//--------------------
+// Use the some Method to Check that Any Elements in an Array Meet a Criteria
+
+/* Question
+Use the some method inside the checkPositive function to check if any element in arr is positive. The function should return a Boolean value.
+*/
+
+
+function checkPositive1(arr) {
+    // Add your code below this line
+    //it is enough if even only one passes the condition
+    return arr.some(item => item > 0)
+  
+    // Add your code above this line
+  }
+  console.log(checkPositive1([1, 2, 3, -4, 5])); // true
