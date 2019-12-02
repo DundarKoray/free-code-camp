f533cf92923e0fb3863eb1d8e3a5d35d2ede1e42
Koray Dundar <koraydundar_40@hotmail.com>
2019-12-02 12:37:00 +0200

    solved a question

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -162,3 +162,35 @@ function translatePigLatin(str) {
 }
 
 console.log(translatePigLatin("consonant"));
+
+
+//--------------------
+// Search and Replace
+
+
+/* Question
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note
+Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+*/
+
+function myReplace(str, before, after) {
+  const myArr = str.split(" ");
+  const [wordToReplace] = myArr.filter(item => item === before);
+  return wordToReplace[0].toUpperCase() !== wordToReplace[0]
+    ? myArr.map(item => (item === before ? after : item)).join(" ")
+    : myArr
+        .map(item =>
+          item === before ? after[0].toUpperCase() + after.slice(1) : item
+        )
+        .join(" ");
+}
+
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
