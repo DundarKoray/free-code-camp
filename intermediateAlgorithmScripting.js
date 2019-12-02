bfd547c57fc9cc3398a5e472d8bd2f892cb52939
Koray Dundar <koraydundar_40@hotmail.com>
2019-12-02 13:33:23 +0200

    solved a question

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -259,7 +259,7 @@ function fearNotLetter(str) {
   for (var i = 0; i < str.length; i++) {
     /* code of current character */
     var code = str.charCodeAt(i);
-    console.log(code)
+    // console.log(code)
     /* if code of current character is not equal to first character + no of iteration
         hence character has been escaped */
     if (code !== str.charCodeAt(0) + i) {
@@ -271,3 +271,31 @@ function fearNotLetter(str) {
 }
 
 fearNotLetter("abce");
+
+
+//--------------------
+// Sorted Union
+
+
+/* Question
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
+*/
+
+function uniteUnique(...arrays) {
+  //make an array out of the given arrays and flatten it (using the spread operator)
+  const flatArray = [].concat(...arrays);
+
+  // create a Set which clears any duplicates since it's a regulat set and not a multiset
+  return [...new Set(flatArray)];
+}
+
+// test here
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
