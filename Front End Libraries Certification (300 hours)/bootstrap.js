bca9223e2dd9bb2a51e62e37690d9563357e31cf
Koray Dundar <koraydundar_40@hotmail.com>
2019-12-10 12:50:49 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/bootstrap.js
+++ Front End Libraries Certification (300 hours)/bootstrap.js
@@ -373,3 +373,33 @@ Nest all three of your checkboxes in a <div class="row"> element. Then nest each
     <input class="form-control" type="text" placeholder="cat photo URL" required>
     <button class="btn btn-primary" type="submit"><i class="fa fa-paper-plane"></i> Submit</button>
 */
+
+
+
+
+
+
+
+
+
+
+// Line up Form Elements Responsively with Bootstrap
+
+/******** Question
+Now let's get your form input and your submission button on the same line. We'll do this the same way we have previously: by using a div element with the class row, and other div elements within it using the col-xs-* class.
+
+Nest both your form's text input and submit button within a div with the class row. Nest your form's text input within a div with the class of col-xs-7. Nest your form's submit button in a div with the class col-xs-5.
+
+This is the last challenge we'll do for our Cat Photo App for now. We hope you've enjoyed learning Font Awesome, Bootstrap, and responsive design!
+*/
+
+/********* Answer
+    <div class="row"> 
+      <div class="col-xs-7">
+        <input type="text" class="form-control" placeholder="cat photo URL" required>
+      </div>
+      <div class="col-xs-5">
+        <button type="submit" class="btn btn-primary"><i class="fa fa-paper-plane"></i> Submit</button>
+      </div>
+    </div>
+*/
