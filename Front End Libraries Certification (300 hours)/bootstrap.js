c395c298a9b2afad5283f9d4f5513ea2748e1075
Koray Dundar <koraydundar_40@hotmail.com>
2019-12-10 12:10:09 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/bootstrap.js
+++ Front End Libraries Certification (300 hours)/bootstrap.js
@@ -241,3 +241,33 @@ Here's how you would do this with the "Top 3 things cats hate" element:
   <p>Things cats <span class="text-danger">love</span>:</p>
 */
 
+
+
+
+
+
+
+
+//--------------------
+// Create a Custom Heading
+
+/******** Question
+We will make a simple heading for our Cat Photo App by putting the title and relaxing cat image in the same row.
+
+Remember, Bootstrap uses a responsive grid system, which makes it easy to put elements into rows and specify each element's relative width. Most of Bootstrap's classes can be applied to a div element.
+
+Nest your first image and your h2 element within a single <div class="row"> element. Nest your h2 element within a <div class="col-xs-8"> and your image in a <div class="col-xs-4"> so that they are on the same line.
+
+Notice how the image is now just the right size to fit along the text?
+*/
+
+/********* Answer
+  <div class="row">
+    <div class="col-xs-8">
+      <h2 class="text-primary text-center">CatPhotoApp</h2>
+    </div>
+    <div class="col-xs-4">
+      <a href="#"><img class="img-responsive thick-green-border" src="https://bit.ly/fcc-relaxing-cat" alt="A cute orange cat lying on its back."></a>
+    </div>
+  </div>
+*/
