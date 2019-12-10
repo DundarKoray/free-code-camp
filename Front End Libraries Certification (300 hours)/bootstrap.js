34cc7a540e78d781b1329a26ed13e0355be88edf
Koray Dundar <koraydundar_40@hotmail.com>
2019-12-10 15:04:19 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/bootstrap.js
+++ Front End Libraries Certification (300 hours)/bootstrap.js
@@ -577,3 +577,44 @@ Nest three button elements within each of your well div elements.
   </div>
 </div>
 */
+
+
+
+
+
+
+
+
+
+//--------------------
+// Apply the Default Bootstrap Button Style
+
+/******** Question
+Bootstrap has another button class called btn-default.
+Apply both the btn and btn-default classes to each of your button elements.
+*/
+
+/********* Answer
+ <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <div class="well">
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="well">
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+      </div>
+    </div>
+  </div>
+</div>
+*/
+
+
+
