3ac3b99b81cc188697cae8ea83570a18ca4ac40d
Koray Dundar <koraydundar_40@hotmail.com>
2019-12-10 12:01:29 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/bootstrap.js
+++ Front End Libraries Certification (300 hours)/bootstrap.js
@@ -216,3 +216,28 @@ Finally, remove the "smaller-image" class from your first img element and replac
 
 
 
+
+
+
+
+//--------------------
+// Use a span to Target Inline Elements
+
+/******** Question
+You can use spans to create inline elements. Remember when we used the btn-block class to make the button fill the entire row?
+
+That illustrates the difference between an "inline" element and a "block" element.
+
+By using the inline span element, you can put several elements on the same line, and even style different parts of the same line differently.
+
+Nest the word "love" in your "Things cats love" element below within a span element. Then give that span the class text-danger to make the text red.
+
+Here's how you would do this with the "Top 3 things cats hate" element:
+
+<p>Top 3 things cats <span class="text-danger">hate:</span></p>
+*/
+
+/********* Answer
+  <p>Things cats <span class="text-danger">love</span>:</p>
+*/
+
