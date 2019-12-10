20ae03bbebc45f30b2e5ee20379ac18fe53e3259
Koray Dundar <koraydundar_40@hotmail.com>
2019-12-10 14:28:59 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/bootstrap.js
+++ Front End Libraries Certification (300 hours)/bootstrap.js
@@ -505,3 +505,33 @@ Create two div elements within your row, both with the class col-xs-6.
     </div>
   </div>
 */
+
+
+
+
+
+
+
+
+//--------------------
+// Create Bootstrap Wells
+
+/******** Question
+Bootstrap has a class called well that can create a visual sense of depth for your columns.
+
+Nest one div element with the class well within each of your col-xs-6 div elements.
+*/
+
+/********* Answer
+  <div class="container-fluid">
+    <h3 class="text-primary text-center">jQuery Playground</h3>
+    <div class="row">
+      <div class="col-xs-6">
+        <div class="well"></div>
+      </div>
+      <div class="col-xs-6">
+        <div class="well"></div>
+      </div>
+    </div>
+  </div>
+*/
