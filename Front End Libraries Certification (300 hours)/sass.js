268b6bdc303898a801b678ee60518ca58472bb5f
Koray Dundar <koraydundar_40@hotmail.com>
2019-12-17 11:02:26 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/sass.js
+++ Front End Libraries Certification (300 hours)/sass.js
@@ -132,3 +132,44 @@ If $val is not light, medium, or heavy, the border should be set to none.
 
 <div id="box">Hello</div>
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------
+// Use @for to Create a Sass Loop
+
+/******** Question
+Write a @for directive that takes a variable $j that goes from 1 to 6.
+
+It should create 5 classes called .text-1 to .text-5 where each has a font-size set to 10px multiplied by the index.
+*/
+
+/********* Answer
+<style type='text/sass'>
+
+    @for $j from 1 through 6 {
+        .text-#{$j} { font-size: (10 * $j)px;}
+    } 
+
+
+</style>
+
+<p class="text-1">Hello</p>
+<p class="text-2">Hello</p>
+<p class="text-3">Hello</p>
+<p class="text-4">Hello</p>
+<p class="text-5">Hello</p>
+
+*/
