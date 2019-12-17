09c2d8f1ea30f9096da08b6a741c363e3689000a
Koray Dundar <koraydundar_40@hotmail.com>
2019-12-17 10:21:21 +0200

    solved a  question

--- Front End Libraries Certification (300 hours)/sass.js
+++ Front End Libraries Certification (300 hours)/sass.js
@@ -83,3 +83,52 @@ Write a mixin for border-radius and give it a $radius parameter. It should use a
 <div id="awesome"></div>
 
 */
+
+
+
+
+
+
+
+//--------------------
+// Use @if and @else to Add Logic To Your Styles
+
+/******** Question
+Create a mixin called border-stroke that takes a parameter $val. The mixin should check for the following conditions using @if, @else if, and @else:
+
+light - 1px solid black
+medium - 3px solid black
+heavy - 6px solid black
+If $val is not light, medium, or heavy, the border should be set to none.
+*/
+
+/********* Answer
+<style type='text/sass'>
+
+  
+
+  @mixin border-stroke($val) {
+    @if $val == light {
+      border: 1px solid black;
+    }
+
+    @else if $val == medium {
+      border: 3px solid black;
+    }
+
+    @else if $val == heavy {
+      border: 6px solid black;
+    }
+
+    @else {
+      border: none;
+    } 
+  }
+
+  #box {
+    @include border-stroke(light)
+  }
+</style>
+
+<div id="box">Hello</div>
+*/
