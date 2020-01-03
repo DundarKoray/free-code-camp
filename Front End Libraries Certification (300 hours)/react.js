a76d5373cd74722723dc602d0a40d1609f40e2f4
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-03 21:40:24 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -517,3 +517,49 @@ class ShoppingCart extends React.Component {
   }
 };
 */
+
+
+
+
+
+
+//--------------------
+// Access Props Using this.props
+
+/******** Question
+Render an instance of the ReturnTempPassword component in the parent component ResetPassword. Here, give ReturnTempPassword a prop of tempPassword and assign it a value of a string that is at least 8 characters long. Within the child, ReturnTempPassword, access the tempPassword prop within the strong tags to make sure the user sees the temporary password.
+*/
+
+/********* Answer
+class ReturnTempPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
+        </div>
+    );
+  }
+};
+
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          <h2>Reset Password</h2>
+          <h3>We've generated a new temporary password for you.</h3>
+          <h3>Please reset this password from your account settings ASAP.</h3>
+          <ReturnTempPassword tempPassword="abcdabcd" />
+        </div>
+    );
+  }
+};
+
+*/
