126bafabf97e8e8f7126e76613fefdef378ed113
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-03 21:29:31 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -486,3 +486,34 @@ class ShoppingCart extends React.Component {
   }
 };
 */
+
+
+//--------------------
+// Use PropTypes to Define the Props You Expect
+
+/******** Question
+Define propTypes for the Items component to require quantity as a prop and verify that it is of type number.
+*/
+
+/********* Answer
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+
+// change code below this line
+Items.propTypes = {quantity: PropTypes.number.isRequired}
+// change code above this line
+
+Items.defaultProps = {
+  quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items />
+  }
+};
+*/
