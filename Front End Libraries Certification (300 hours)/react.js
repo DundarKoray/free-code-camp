d306b81cc0f248bbc7ab446384ea77211efe6d2c
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-03 14:56:30 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -96,3 +96,29 @@ const JSX = (
   </div>
 );
 */
+
+
+
+
+
+
+//--------------------
+// Create a Stateless Functional Component
+
+/******** Question
+The code editor has a function called MyComponent. Complete this function so it returns a single div element which contains some string of text.
+
+Note: The text is considered a child of the div element, so you will not be able to use a self-closing tag.
+*/
+
+/********* Answer
+const MyComponent = function() {
+  // change code below this line
+  return (
+  <div>Hello React</div>
+  )
+
+  // change code above this line
+}
+
+*/
