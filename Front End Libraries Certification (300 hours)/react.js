68b4a8046f1e959ef2ec5f42568a41a74561a108
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-03 14:18:26 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -12,3 +12,25 @@ The current code uses JSX to assign a div element to the constant JSX. Replace t
 /********* Answer
 const JSX = <h1>Hello JSX!</h1>;
 */
+
+
+
+//--------------------
+// Create a Complex JSX Element
+
+/******** Question
+The current code uses JSX to assign a div element to the constant JSX. Replace the div with an h1 element and add the text Hello JSX! inside it.
+*/
+
+/********* Answer
+// write your code here
+const JSX = <div> 
+    <h1>TOGG electric car</h1>
+    <p>Turkey introduces its first electric car which will be available by 2021</p>
+    <ul>
+        <li>Sedan</li>
+        <li>SUV</li>
+        <li>Hatchback</li>
+    </ul>
+</div>
+*/
