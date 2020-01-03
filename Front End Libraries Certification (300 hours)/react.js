ba7e05cabd3cc0f261aa1ecd73aa5a9c6c03326b
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-03 20:49:10 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -428,3 +428,28 @@ class ToDo extends React.Component {
   }
 };
 */
+
+
+
+
+
+
+
+//--------------------
+// Use Default Props
+
+/******** Question
+The code editor shows a ShoppingCart component. Define default props on this component which specify a prop items with a value of 0.
+*/
+
+/********* Answer
+const ShoppingCart = (props) => {
+  return (
+    <div>
+      <h1>Shopping Cart Component</h1>
+    </div>
+  )
+};
+// change code below this line
+ShoppingCart.defaultProps = { items: 0}
+*/
