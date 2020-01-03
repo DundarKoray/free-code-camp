ff9a1512581693bc9e4f9fe6db3966413bd9d97d
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-03 14:42:48 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -75,3 +75,24 @@ const JSX = (
   </div>
 );
 */
+
+
+
+
+
+//--------------------
+// Learn About Self-Closing JSX Tags
+
+/******** Question
+Fix the errors in the code editor so that it is valid JSX and successfully transpiles. Make sure you don't change any of the content - you only need to close tags where they are needed.
+*/
+
+/********* Answer
+const JSX = (
+  <div>
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+  </div>
+);
+*/
