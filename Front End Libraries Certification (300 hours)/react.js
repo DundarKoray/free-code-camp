441f13428b1b00a64e5e2a7e23db1d0ca5e1a061
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-03 14:32:29 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -34,3 +34,25 @@ const JSX = <div>
     </ul>
 </div>
 */
+
+
+
+
+
+//--------------------
+// Render HTML Elements to the DOM
+
+/******** Question
+The code editor has a simple JSX component. Use the ReactDOM.render() method to render this component to the page. You can pass defined JSX elements directly in as the first argument and use document.getElementById() to select the DOM node to render them to. There is a div with id='challenge-node' available for you to use. Make sure you don't change the JSX constant.
+*/
+
+/********* Answer
+const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+// change code below this line
+ReactDOM.render(JSX, document.getElementById("challenge-node"))
+*/
