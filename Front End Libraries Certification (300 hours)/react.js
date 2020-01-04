649e853330c8f17125553b86204ee0aacd838894
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-04 16:51:45 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -670,5 +670,40 @@ class MyComponent extends React.Component {
     );
   }
 };
+*/
+
+
+
+
+
+
+//--------------------
+// Render State in the User Interface Another Way
+
+/******** Question
+In the MyComponent render method, define a const called name and set it equal to the name value in the component's state. Because you can write JavaScript directly in this part of the code, you don't have to enclose this reference in curly braces.
+
+Next, in the return statement, render this value in an h1 tag using the variable name. Remember, you need to use the JSX syntax (curly braces for JavaScript) in the return statement.
+*/
+
+/********* Answer
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    // change code below this line
+    const name = this.state.name
+    // change code above this line
+    return (
+      <div>
+        <h1>{name}</h1>
+      </div>
+    );
+  }
+};
 
 */
