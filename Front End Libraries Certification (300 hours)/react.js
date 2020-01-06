db9c5d10dda7499fadf34cd641ac626a1ec05764
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-06 22:05:41 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -1520,6 +1520,43 @@ class MyComponent extends React.Component {
         
   }
 };
+*/
 
+
+
+
+
+
+//--------------------
+//  Use && for a More Concise Conditional
+
+/******** Question
+Solve the previous example again, so the h1 only renders if display is true, but use the && logical operator instead of an if/else statement.
+*/
+
+/********* Answer
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState(state => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    // change code below this line
+    return (
+              <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         {this.state.display && <h1>Displayed!</h1>}
+       </div>
+    );
+  }
+};
 
 */
