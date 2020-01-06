031b7b1636349fba869b0c0fd729ccef66934775
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-06 15:32:31 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react.js
+++ Front End Libraries Certification (300 hours)/react.js
@@ -1162,3 +1162,44 @@ class MyComponent extends React.Component {
 };
 
 */
+
+
+
+
+
+
+
+
+
+//--------------------
+//  Use the Lifecycle Method componentDidMount
+
+/******** Question
+There is a mock API call in componentDidMount(). It sets state after 2.5 seconds to simulate calling a server to retrieve data. This example requests the current total active users for a site. In the render method, render the value of activeUsers in the h1. Watch what happens in the preview, and feel free to change the timeout to see the different effects.
+*/
+
+/********* Answer
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: 0
+    };
+  }
+  componentDidMount() {
+    setTimeout( () => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Active Users: {this.state.activeUsers}</h1>
+      </div>
+    );
+  }
+};
+
+*/
