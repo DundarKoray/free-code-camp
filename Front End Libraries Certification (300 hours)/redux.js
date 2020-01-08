a15558a03df41ffabd4c9b408d08be6e663477fd
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-08 11:59:50 +0200

    solved a redux question

--- Front End Libraries Certification (300 hours)/redux.js
+++ Front End Libraries Certification (300 hours)/redux.js
@@ -50,3 +50,25 @@ const store = Redux.createStore(
 const currentState = store.getState()
 }
 */
+
+
+
+
+
+
+
+
+
+//--------------------
+// Define a Redux Action
+
+/******** Question
+Writing a Redux action is as simple as declaring an object with a type property. Declare an object action and give it a property type set to the string 'LOGIN'.
+*/
+
+/********* Answer
+// Define an action here:
+const action = {
+    type: 'LOGIN'
+}
+*/
