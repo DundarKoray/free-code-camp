aa794cc39ab9bd7802fb79bf0161f0bdb7fa28cd
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-09 17:03:57 +0200

    solved a redux question

--- Front End Libraries Certification (300 hours)/redux.js
+++ Front End Libraries Certification (300 hours)/redux.js
@@ -239,3 +239,72 @@ const logoutUser = () => {
 
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------
+// Use const for Action Types
+
+/******** Question
+Declare LOGIN and LOGOUT as const values and assign them to the strings 'LOGIN' and 'LOGOUT', respectively. Then, edit the authReducer() and the action creators to reference these constants instead of string values.
+
+Note: It's generally a convention to write constants in all uppercase, and this is standard practice in Redux as well.
+*/
+
+/********* Answer
+// change code below this line
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+// change code above this line
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+
+  switch (action.type) {
+
+    case LOGIN:
+      return {
+        authenticated: true
+      }
+
+    case LOGOUT:
+      return {
+        authenticated: false
+      }
+
+    default:
+      return state;
+
+  }
+
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: LOGIN
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT
+  }
+};
+*/
