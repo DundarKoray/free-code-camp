3b1dc868372b8b215cbb2ac3700e52ac1273042a
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-13 12:06:36 +0200

    solved a redux question

--- Front End Libraries Certification (300 hours)/redux.js
+++ Front End Libraries Certification (300 hours)/redux.js
@@ -653,3 +653,57 @@ const decAction = () => {
 const store = Redux.createStore(counterReducer); // define the Redux store here, passing in your reducers
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------
+//   Never Mutate State
+
+/******** Question
+There is a store and reducer in the code editor for managing to-do items. Finish writing the ADD_TO_DO case in the reducer to append a new to-do to the state. There are a few ways to accomplish this with standard JavaScript or ES6. See if you can find a way to return a new array with the item from action.todo appended to the end.
+*/
+
+/********* Answer
+const ADD_TO_DO = 'ADD_TO_DO';
+
+// A list of strings representing tasks to do:
+const todos = [
+  'Go to the store',
+  'Clean the house',
+  'Cook dinner',
+  'Learn to code',
+];
+
+const immutableReducer = (state = todos, action) => {
+  switch(action.type) {
+    case ADD_TO_DO:
+      return [...todos,action.todo]
+      // don't mutate state here or the tests will fail
+      
+    default:
+      return state;
+  }
+};
+
+// an example todo argument would be 'Learn React',
+const addToDo = (todo) => {
+  return {
+    type: ADD_TO_DO,
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
+*/
