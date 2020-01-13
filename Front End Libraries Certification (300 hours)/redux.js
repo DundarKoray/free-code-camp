e1bb4ecea15760c76d954519da3ca95ea93adbff
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-13 12:19:57 +0200

    solved a redux question

--- Front End Libraries Certification (300 hours)/redux.js
+++ Front End Libraries Certification (300 hours)/redux.js
@@ -707,3 +707,38 @@ const addToDo = (todo) => {
 
 const store = Redux.createStore(immutableReducer);
 */
+
+
+
+
+
+
+
+
+
+
+//--------------------
+//   Use the Spread Operator on Arrays
+
+/******** Question
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
+  switch(action.type) {
+    case 'ADD_TO_DO':
+      // don't mutate state here or the tests will fail
+      let arr = [...state, action.todo] 
+      return arr
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: 'ADD_TO_DO',
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
+
+*/
