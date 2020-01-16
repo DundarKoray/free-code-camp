61728c71bb9acecacdeadfdfa63909cf7f9c37ff
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-16 10:37:07 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react_and_redux.js
+++ Front End Libraries Certification (300 hours)/react_and_redux.js
@@ -96,3 +96,51 @@ class DisplayMessages extends React.Component {
   };
   
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------
+// Extract State Logic to Redux
+
+/******** Question
+First, define an action type 'ADD' and set it to a const ADD. Next, define an action creator addMessage() which creates the action to add a message. You'll need to pass a message to this action creator and include the message in the returned action.
+
+Then create a reducer called messageReducer() that handles the state for the messages. The initial state should equal an empty array. This reducer should add a message to the array of messages held in state, or return the current state. Finally, create your Redux store and pass it the reducer.
+*/
+
+/********* Answer
+// define ADD, addMessage(), messageReducer(), and store here:
+const ADD = 'ADD';
+
+const addMessage = (message) => {
+    return {
+        type: ADD,
+        message
+    }
+}
+
+const messageReducer = (previousState = [], action) => {
+    switch(action.type){
+        case ADD:
+            return [action.message]
+            break;
+
+        default:
+            return previousState
+    }
+} 
+
+
+const store = Redux.createStore(messageReducer);
+*/
