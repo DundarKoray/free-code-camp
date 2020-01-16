83ab36ca2bc2f849023c8c1ca091656dbbec7e63
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-16 10:26:32 +0200

    solved a question

--- Front End Libraries Certification (300 hours)/react_and_redux.js
+++ Front End Libraries Certification (300 hours)/react_and_redux.js
@@ -31,3 +31,68 @@ class DisplayMessages extends React.Component {
 };
 
 */
+
+
+
+
+
+
+
+
+
+
+//--------------------
+// Manage State Locally First
+
+/******** Question
+First, in the render() method, have the component render an input element, button element, and ul element. When the input element changes, it should trigger a handleChange() method. Also, the input element should render the value of input that's in the component's state. The button element should trigger a submitMessage() method when it's clicked.
+
+Second, write these two methods. The handleChange() method should update the input with what the user is typing. The submitMessage() method should concatenate the current message (stored in input) to the messages array in local state, and clear the value of the input.
+
+Finally, use the ul to map over the array of messages and render it to the screen as a list of li elements.
+*/
+
+/********* Answer
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+      messages: []
+    }
+
+    this.submitMessage = this.submitMessage.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  // add handleChange() and submitMessage() methods here
+  submitMessage(event) {
+    event.preventDefault()
+    this.setState({
+      input: '',
+      messages: [...this.state.messages, this.state.input]
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        
+        <ul>{this.state.messages.map(message => {
+            return <li>{message}</li>
+          })}</ul>
+          <input value={this.state.input} onChange={this.handleChange} />
+          <button type='submit' onClick={this.submitMessage}>Submit</button>
+          
+        </div>
+      );
+    }
+  };
+  
+*/
