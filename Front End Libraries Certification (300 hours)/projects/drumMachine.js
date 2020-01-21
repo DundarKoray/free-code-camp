181f32c7fb02b7d89ba7a53e97117bf45502afce
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-21 21:19:43 +0200

    started drumMachine project, completed step 1, 2

---
+++ Front End Libraries Certification (300 hours)/projects/drumMachine.js
@@ -0,0 +1,21 @@
+/*
+import React from "react";
+
+class DrumMachine extends React.Component {
+  render() {
+    return (
+      <div id="drum-machine">
+        <div class="drum-pad">
+          
+        </div>
+      </div>
+    ) 
+      
+  }
+}
+
+export default DrumMachine;
+
+
+
+*/
