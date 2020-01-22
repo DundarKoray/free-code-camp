3e4f39ec3da206aad45713aed27879e92636feb2
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-22 22:11:49 +0200

    solved a D3 question

--- Data Visualization Certification/D3.js
+++ Data Visualization Certification/D3.js
@@ -128,3 +128,39 @@ Change the text() method so that each h2 element displays the corresponding valu
   </script>
 </body>
 */
+
+
+
+
+
+
+
+
+
+
+//--------------------
+// Add Inline Styling to Elements
+
+/******** Question
+Add the style() method to the code in the editor to make all the displayed text have a font-family of verdana.
+*/
+
+/********* Answer
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    d3.select("body").selectAll("h2")
+      .data(dataset)
+      .enter()
+      .append("h2")
+      .text((d) => (d + " USD"))
+      // Add your code below this line
+      .style("font-family","verdana")
+
+
+      // Add your code above this line
+  </script>
+</body>
+
+*/
