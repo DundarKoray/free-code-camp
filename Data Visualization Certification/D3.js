0746562d9651d02ebe1ec1081fe2f7145613820b
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-22 21:57:06 +0200

    solved a question

--- Data Visualization Certification/D3.js
+++ Data Visualization Certification/D3.js
@@ -20,3 +20,38 @@ Use the select method to select the body tag in the document. Then append an h1 
   </script>
 </body>
 */
+
+
+
+
+
+
+
+
+
+
+//--------------------
+// Select a Group of Elements with D3
+
+/******** Question
+Select all of the li tags in the document, and change their text to "list item" by chaining the .text() method.
+*/
+
+/********* Answer
+<body>
+  <ul>
+    <li>Example</li>
+    <li>Example</li>
+    <li>Example</li>
+  </ul>
+  <script>
+    // Add your code below this line
+
+  d3.selectAll("li").text("list item")
+
+
+    // Add your code above this line
+  </script>
+</body>
+
+*/
