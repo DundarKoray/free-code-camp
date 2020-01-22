cc4e8b8c7ab1c04904fe452d393a7c98c81214ac
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-22 22:04:06 +0200

    solved a question

--- Data Visualization Certification/D3.js
+++ Data Visualization Certification/D3.js
@@ -55,3 +55,40 @@ Select all of the li tags in the document, and change their text to "list item" 
 </body>
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------
+// Work with Data in D3
+
+/******** Question
+Select the body node, then select all h2 elements. Have D3 create and append an h2 tag for each item in the dataset array. The text in the h2 should say "New Title". Your code should use the data() and enter() methods.
+*/
+
+/********* Answer
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    // Add your code below this line
+    d3.select("body").selectAll("h2")
+      .data(dataset)
+      .enter()
+      .append("h2")
+      .text("New Title")
+    // Add your code above this line
+  </script>
+</body>
+*/
