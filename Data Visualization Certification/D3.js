b7939ea0ab66fb919c8bb0aa34179b9d4b701c64
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-23 19:03:46 +0200

    solved a question

--- Data Visualization Certification/D3.js
+++ Data Visualization Certification/D3.js
@@ -428,3 +428,47 @@ Add a rect shape to the svg using append(), and give it a width attribute of 25 
 
 
 
+
+
+
+
+
+//--------------------
+//  Create a Bar for Each Data Point in the Set
+
+/******** Question
+Use the data(), enter(), and append() methods to create and append a rect for each item in dataset. The bars should display all on top of each other, this will be fixed in the next challenge.
+*/
+
+/********* Answer
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.selectAll("rect")
+       // Add your code below this line
+      .data(dataset).enter().append("rect")
+
+
+       // Add your code above this line
+       .attr("x", 0)
+       .attr("y", 0)
+       .attr("width", 25)
+       .attr("height", 100);
+  </script>
+</body>
+*/
+
+
+
+
+
+
