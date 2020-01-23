bbe9f50351abd2d068be42f8a8fab3ec55942641
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-23 15:58:33 +0200

    solved a D3.js question

--- Data Visualization Certification/D3.js
+++ Data Visualization Certification/D3.js
@@ -205,3 +205,45 @@ You can use if-else logic, or the ternary operator.
   </script>
 </body>
 */
+
+
+
+
+
+
+
+
+
+//--------------------
+// Add Classes with D3
+
+/******** Question
+Add the attr() method to the code in the editor and put a class of bar on the div elements.
+*/
+
+/********* Answer
+<style>
+  .bar {
+    width: 25px;
+    height: 100px;
+    display: inline-block;
+    background-color: blue;
+  }
+</style>
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    d3.select("body").selectAll("div")
+      .data(dataset)
+      .enter()
+      .append("div")
+      // Add your code below this line
+      .attr("class", "bar")
+
+
+      // Add your code above this line
+  </script>
+</body>
+
+*/
