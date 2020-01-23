cacf26f5c00533cb6742da29fa0308ce7d122496
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-23 16:50:02 +0200

    solved a question

--- Data Visualization Certification/D3.js
+++ Data Visualization Certification/D3.js
@@ -342,3 +342,48 @@ Multiplying each data point by the same constant only alters the scale. It's lik
 
 
 
+
+
+
+
+//--------------------
+//  Learn About SVG in D3
+
+/******** Question
+Add an svg node to the body using append(). Give it a width attribute set to the provided w constant and a height attribute set to the provided h constant using the attr() or style() methods for each. You'll see it in the output because there's a background-color of pink applied to it in the style tag.
+
+Note
+When using attr() width and height attributes do not have units. This is the building block of scaling - the element will always have a 5:1 width to height ratio, no matter what the zoom level is.
+*/
+
+/********* Answer
+  <style>
+  svg {
+    background-color: pink;
+  }
+</style>
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+    // Add your code below this line
+    svg.data(dataset)
+     .append("svg")
+     .style("width", w)
+     .style("height", h)
+    // Add your code above this line
+  </script>
+</body>
+*/
+
+
+
+
+
+
+
+
