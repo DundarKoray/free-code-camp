edbd2081b807d522335e3596dc1f16bd9f7b61ef
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-26 12:02:37 +0200

    solved a question

--- Data Visualization Certification/D3.js
+++ Data Visualization Certification/D3.js
@@ -569,11 +569,63 @@ Remember that multiplying all data points by the same constant scales the data (
        });
   </script>
 </body>
-
-
-
 */
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------
+//  Invert SVG Elements
+
+/******** Question
+Change the callback function for the y attribute to set the bars right-side-up. Remember that the height of the bar is 3 times the data value d.
+
+Note
+In general, the relationship is y = h - m * d, where m is the constant that scales the data points.
+*/
+
+/********* Answer
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.selectAll("rect")
+       .data(dataset)
+       .enter()
+       .append("rect")
+       .attr("x", (d, i) => i * 30)
+       .attr("y", (d, i) => {
+         // Add your code below this line
+        return  i = h - 3 * d
+
+
+         // Add your code above this line
+       })
+       .attr("width", 25)
+       .attr("height", (d, i) => 3 * d);
+  </script>
+</body>
+
+*/
 
 
 
