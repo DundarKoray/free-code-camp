d3e9f2e0c47d6004dd36869afca4cbc26ee9d60a
Koray Dundar <koraydundar_40@hotmail.com>
2020-01-29 21:17:51 +0200

    solved a question

--- Data Visualization Certification/D3.js
+++ Data Visualization Certification/D3.js
@@ -1151,3 +1151,41 @@ You can chain the domain() and range() methods onto the scale variable.
 </body>
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------
+//   Use the d3.max and d3.min Functions to Find Minimum and Maximum Values in a Dataset
+
+/******** Question
+The positionData variable holds a 3-dimensional (3D) array. Use a D3 method to find the maximum value of the z coordinate (the third value) from the arrays and save it in the output variable.
+
+Note
+Fun fact - D3 can plot 3D arrays.
+*/
+
+/********* Answer
+<body>
+  <script>
+    const positionData = [[1, 7, -4],[6, 3, 8],[2, 9, 3]]
+    // Add your code below this line
+
+    const output = d3.max(positionData, d => d[2]); // Change this line
+
+    // Add your code above this line
+
+    d3.select("body")
+      .append("h2")
+      .text(output)
+  </script>
+</body>
+*/
