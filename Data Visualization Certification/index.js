8bb450276db4f9024b2c5ef11e7510490ec375fd
Koray Dundar <koraydundar_40@hotmail.com>
2020-02-02 16:30:38 +0200

    practiced method chaining and attributes d3.js

--- Data Visualization Certification/index.js
+++ Data Visualization Certification/index.js
@@ -2,8 +2,27 @@ const canvas = d3.select('.canvas')
 console.log(canvas)
 
 const svg = canvas.append("svg")
+    .attr("height", "600")
+    .attr("width", "600")
+
 
 //appending elements
 svg.append("rect")
+    .attr("height", 100)
+    .attr("width", 200)
+    .attr("x", 20)
+    .attr("y", 20)
+    .attr("fill", "blue")
+
 svg.append("circle")
+    .attr("r", 50)
+    .attr("cx", 300)
+    .attr("cy", 70)
+
 svg.append("line")
+    .attr("x1", 370)
+    .attr("x2", 400)
+    .attr("y1", 20)
+    .attr("y2", 120)
+    .attr("stroke", "pink")
+    .attr("stroke-width", 2)
