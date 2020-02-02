6683a98f7075972d4ada7c52b53d0bac49899165
Koray Dundar <koraydundar_40@hotmail.com>
2020-02-02 21:47:53 +0200

    grouped rect, circle and line

--- Data Visualization Certification/index.js
+++ Data Visualization Certification/index.js
@@ -1,31 +1,32 @@
-const canvas = d3.select('.canvas')
+const canvas = d3.select('.canvas');
 console.log(canvas)
 
 const svg = canvas.append("svg")
     .attr("height", "600")
-    .attr("width", "600")
+    .attr("width", "600");
 
+const group = svg.append("g");
 
 //appending elements
-svg.append("rect")
+group.append("rect")
     .attr("height", 100)
     .attr("width", 200)
     .attr("x", 20)
     .attr("y", 20)
-    .attr("fill", "blue")
+    .attr("fill", "blue");
 
-svg.append("circle")
+group.append("circle")
     .attr("r", 50)
     .attr("cx", 300)
-    .attr("cy", 70)
+    .attr("cy", 70);
 
-svg.append("line")
+group.append("line")
     .attr("x1", 370)
     .attr("x2", 400)
     .attr("y1", 20)
     .attr("y2", 120)
     .attr("stroke", "pink")
-    .attr("stroke-width", 2)
+    .attr("stroke-width", 2);
 
 svg.append("text")
     .attr("x", 200)
@@ -33,4 +34,4 @@ svg.append("text")
     .attr("fill", "red")
     .text("hello ninjas!")
     .style("font-family", "arial")
-    .style("font-size", 30)
+    .style("font-size", 30);
